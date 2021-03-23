926a133301f521203fbdff1b2b3bbbb41641a296
David Robison <david@robisonweb.dev>
2021-03-23 08:42:54 -0600

    imported and appended addProjectsBtn, projectsList, and allProjects

--- src/projectsContainer.js
+++ src/projectsContainer.js
@@ -1,7 +1,16 @@
+import addProject from './addProject';
+import addProjectBtn from './addProjectBtn';
+import projectsList from './projectsList';
+import allProjects from './allProjects';
+
 function projectsContainer() {
     const projectsContainer = document.createElement('div');
 
     projectsContainer.setAttribute('id', 'projectsContainer');
+
+    projectsContainer.appendChild(allProjects());
+    projectsContainer.appendChild(projectsList());
+    projectsContainer.appendChild(addProjectBtn());
 
     return projectsContainer;
 }
