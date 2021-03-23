93222eb07a58fe06830777815d56d76bd395ed83
David Robison <david@robisonweb.dev>
2021-03-23 13:35:23 -0600

    Temp save

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import header from './header';
+import content from './content';
+import footer from './footer';
+import addProjectBtn from './addProjectBtn';
+import addProjectForm from './addProjectForm';
+import removeProjectForm from './removeProjectForm';
+
+// import projects from './projects';
+// import addProject from './addProject';
+// import projectsList from './projectsList';
+
+const container = document.querySelector('#container');
+
+// projectsList();
+
+container.appendChild(header('las la-list-ul', 'Todo List'));
+container.appendChild(content());
+container.appendChild(footer('Developed by ', 'David Robison', 'https://github.com/robisonwebdev'));
+
+const addProjectBtnEvent = document.querySelector('#addProjectBtn');
+
+
+addProjectBtnEvent.addEventListener('click', () => {
+    addProjectForm();
+});
+
