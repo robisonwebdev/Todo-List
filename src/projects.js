7769ca67554a91db0a6aebe822622f162d8cb7db
David Robison <david@robisonweb.dev>
2021-03-23 13:33:20 -0600

    Added temp project objects

--- src/projects.js
+++ src/projects.js
@@ -1,3 +1,17 @@
-let projects = [];
+// Modified because of temp projects
+let projects = [
+    {
+        name: 'Home Improvement',
+        dataKey: 'HI_88234'
+    },
+    {
+        name: 'Fix Car',
+        dataKey: 'FC_2126'
+    },
+    {
+        name: 'School',
+        dataKey: 'S_525'
+    }
+];
 
 export default projects;
