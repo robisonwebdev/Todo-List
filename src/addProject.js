d7c3adae1cd380a1204e8e4647fd699b7ec305d6
David Robison <david@robisonweb.dev>
2021-03-24 18:53:46 -0600

    Remove title variable, set title as a param

--- src/addProject.js
+++ src/addProject.js
@@ -1,8 +1,7 @@
 import projects from './projects';
 import project from './projectClass';
 
-function addProject() {
-    const title = document.querySelector('#projectTitle').value;
+function addProject(title) {
     const dataKey = `${title}_${Math.floor((Math.random() * 100000) + 1)}`;
 
     const newProject = new project(title, dataKey);
