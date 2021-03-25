19270c327a1ee1026e4e5cd8d1ec67ef857c61c5
David Robison <david@robisonweb.dev>
2021-03-24 22:25:00 -0600

    Append allProjects and projectList to projectContainer

--- src/refreshProjectsContainer.js
+++ src/refreshProjectsContainer.js
@@ -1,9 +1,16 @@
+import allProjects from "./allProjects";
+import projectsList from "./projectsList";
 import removeProjectsContainerChildren from "./removeProjectsContainerChildren";
 
 function refreshProjectsContainer() {
     removeProjectsContainerChildren();
 
     const projectsContainer = document.querySelector('#projectsContainer');
+
+    projectsContainer.appendChild(allProjects());
+    projectsContainer.appendChild(projectsList());
+
+    return projectsContainer;
 }
 
 export default refreshProjectsContainer;
