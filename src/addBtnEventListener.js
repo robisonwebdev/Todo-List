4f993ed7a7e94067c0b2b9ef9eaf18839f826811
David Robison <david@robisonweb.dev>
2021-03-24 18:52:29 -0600

    when clicked, call removeProjectForm

--- src/addBtnEventListener.js
+++ src/addBtnEventListener.js
@@ -1,11 +1,13 @@
 import addProject from "./addProject";
 import projects from "./projects";
+import removeProjectForm from "./removeProjectForm";
 
 function addBtnEventLinstener(target) {
     target.addEventListener('click', () => {
         const inputValue = document.querySelector('#projectName').value;
 
         addProject(inputValue);
+        removeProjectForm();
     })
 }
 
