b3fa8d2858fe4252380260b2a701247895a43c5e
David Robison <david@robisonweb.dev>
2021-03-24 22:17:59 -0600

    Function that will reload projectContainer

---
+++ src/refreshProjectsContainer.js
@@ -0,0 +1,7 @@
+import removeProjectsContainerChildren from "./removeProjectsContainerChildren";
+
+function refreshProjectsContainer() {
+    removeProjectsContainerChildren();
+
+    const projectsContainer = document.querySelector('#projectsContainer');
+}
