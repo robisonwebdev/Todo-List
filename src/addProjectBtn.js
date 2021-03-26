aeb67073da50bbe5188810867f9e5150e686ee59
David Robison <david@robisonweb.dev>
2021-03-26 09:21:37 -0600

    Called changeDisplay

--- src/addProjectBtn.js
+++ src/addProjectBtn.js
@@ -1,4 +1,5 @@
 import addProjectForm from './addProjectForm';
+import changeDisplay from './changeDisplay';
 
 function addProjectBtn() {
     const addProjectBtn = document.createElement('div');
@@ -15,6 +16,7 @@ function addProjectBtn() {
     addProjectBtn.appendChild(addProjectText);
 
     addProjectBtn.addEventListener('click', () => {
+        changeDisplay('#addProjectBtn', false);
         addProjectForm();
     })
 
