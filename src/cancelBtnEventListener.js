f91b750257a4ed3aad560c91a020bb1f735444be
David Robison <david@robisonweb.dev>
2021-03-26 09:23:23 -0600

    Called changeDisplay

--- src/cancelBtnEventListener.js
+++ src/cancelBtnEventListener.js
@@ -1,8 +1,10 @@
+import changeDisplay from './changeDisplay';
 import removeProjectForm from './removeProjectForm';
 
 function cancelBtnEventListener(target) {
     target.addEventListener('click', () => {
         removeProjectForm();
+        changeDisplay('#addProjectBtn', true);
     })
 }
 
