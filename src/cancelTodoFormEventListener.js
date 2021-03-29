6b4b11267e03407ba2a92ca3c05df4fc29bc43c0
David Robison <david@robisonweb.dev>
2021-03-29 10:16:51 -0600

    Called removeTodoForm on click

---
+++ src/cancelTodoFormEventListener.js
@@ -0,0 +1,12 @@
+import changeDisplay from "./changeDisplay";
+import removeTodoForm from "./removeTodoForm";
+
+function cancelTodoFormEventListener(target) {
+
+    target.addEventListener('click', () => {
+        changeDisplay('#addTodoItemBtn', true);
+        removeTodoForm();
+    })
+}
+
+export default cancelTodoFormEventListener;
