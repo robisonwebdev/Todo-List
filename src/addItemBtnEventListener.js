ba2ab33b00898b88b00503522899d6be5f4c5b5d
David Robison <david@robisonweb.dev>
2021-03-29 09:24:29 -0600

    Call addTodoItemForm on click

--- src/addItemBtnEventListener.js
+++ src/addItemBtnEventListener.js
@@ -1,9 +1,10 @@
+import addTodoItemForm from './addTodoItemForm';
 import changeDisplay from './changeDisplay';
 
 function addItemBtnEventListener(target) {
     target.addEventListener('click', () => {
         changeDisplay(`#${target.id}`, false);
-        // console.log(target.id);
+        addTodoItemForm();
     })
 }
 
