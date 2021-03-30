7c71a9d2c3e791cb23b1d28a12868dfb9abeb5d2
David Robison <david@robisonweb.dev>
2021-03-30 08:14:38 -0600

    Removed console.log

--- src/findProject.js
+++ src/findProject.js
@@ -3,7 +3,6 @@ import projects from "./projects";
 function findProject(dataKey) {
     for (let i = 0; i < projects.length; i++) {
         if (projects[i]['dataKey'] === dataKey) {
-            console.log(projects[i]);
             return projects[i];
         }
     }
