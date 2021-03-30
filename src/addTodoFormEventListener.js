1388aef884e820537baf3127e902630761304f12
David Robison <david@robisonweb.dev>
2021-03-30 08:14:02 -0600

    Imported and called clearTodoCOntainer, findProject, and todoList. Selected dataKey from #todoHeader and passed it to findProject

--- src/addTodoFormEventListener.js
+++ src/addTodoFormEventListener.js
@@ -1,11 +1,17 @@
 import addTodoItem from "./addTodoItem";
+import clearTodoContainer from "./clearTodoContainer";
+import findProject from "./findProject";
+import todoList from "./todoList";
 
 function addTodoFormEventListner(target) {
     target.addEventListener('click', () => {
+        const dataKey = document.querySelector('#todoHeader').getAttribute('dataKey');
         const inputValue = document.querySelector('#todoInput').value;
 
         if (inputValue !== '') {
             addTodoItem();
+            clearTodoContainer();
+            todoList(findProject(dataKey));
         }
     })
 }
