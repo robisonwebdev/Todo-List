59c171fcad4c7c94aadca8c8a9fc426da8d3df27
David Robison <david@robisonweb.dev>
2021-03-31 07:17:43 -0600

    Imported and called clearTodoContainer

--- src/allProjectsEventListener.js
+++ src/allProjectsEventListener.js
@@ -1,7 +1,9 @@
 import allTodos from "./allTodos";
+import clearTodoContainer from './clearTodoContainer';
 
 function allProjectsEventListener(target) {
     target.addEventListener('click', () => {
+        clearTodoContainer();
         allTodos();
     })
 }
