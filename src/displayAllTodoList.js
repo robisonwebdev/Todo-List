b8a5bc631d799cc1d22d622c49b6fc4955275713
David Robison <david@robisonweb.dev>
2021-03-31 07:09:24 -0600

    Created function to grab all todo from all projects and display them in UL

---
+++ src/displayAllTodoList.js
@@ -0,0 +1,31 @@
+import projects from "./projects";
+
+function displayAllTodoList() {
+    const ul = document.createElement('ul');
+    const li = document.createElement('li');
+
+    for (let i = 0; i < projects.length; i++) {
+        for (let j = 0; j < projects[i]['todo'].length; j++) {
+            const item = document.createElement('div');
+            const checkbox = document.createElement('input');
+            const label = document.createElement('label');            
+
+            item.setAttribute('class', 'item');
+            checkbox.setAttribute('type', 'checkbox');
+            checkbox.setAttribute('name', `${projects[i]['dataKey']}_${projects[i]['todo'][j]}`);
+            label.setAttribute('for', `${projects[i]['dataKey']}_${projects[i]['todo'][j]}`);
+
+            label.innerText = projects[i]['todo'][j];
+
+            item.appendChild(checkbox);
+            item.appendChild(label);
+            li.appendChild(item);
+        }
+    }
+
+    ul.appendChild(li);
+
+    return ul;
+}
+
+export default displayAllTodoList;
