62d2ffec8a9a220e0a288b1e5bbe0eb6d50c8940
David Robison <david@robisonweb.dev>
2021-04-01 13:31:43 -0600

    Added if statement to determine which project cata working on

--- src/checkboxEventListener.js
+++ src/checkboxEventListener.js
@@ -2,12 +2,21 @@ import deleteItem from "./deleteItem";
 import clearTodoContainer from './clearTodoContainer';
 import todoList from './todoList';
 import findProject from './findProject';
+import allTodos from "./allTodos";
 
 function checkboxEventListener(target, todo) {
+    
     target.addEventListener('click', () => {
+        const getHeaderData = document.querySelector('#todoHeader').getAttribute('dataKey');
+        
         deleteItem(target.getAttribute('dataKey'), todo);
         clearTodoContainer();
-        todoList(findProject(target.getAttribute('dataKey')));
+
+        if (getHeaderData === 'all_Projects') {
+            allTodos();
+        } else {
+            todoList(findProject(target.getAttribute('dataKey')));
+        }
     })
 }
 
